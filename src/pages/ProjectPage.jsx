550130f59ab830fc33f6f86821178e8bee582c71
segun olowoyeye <segunolowoyeye@gmail.com>
2024-06-29 23:22:36 +0100

    another commited one

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -30,72 +30,72 @@ const ProjectPage = () => {
               Click here
             </a>
           </p>
-          <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
-            <a
-              href="https://segzyp1000.github.io/loansite/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={Project1} alt="" />
-            </a>
-            <p className="flex flex-col items-center w-1/2 mt-5">
-              Built a loan website replica using React, JavaScript, HTML, CSS,
-              and Bootstrap, demonstrating my skills in creating responsive and
-              dynamic web applications
-              <a
-                href="https://segzyp1000.github.io/loansite"
-                className="bg-white text-black rounded p-1 mt-3"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Click here
-              </a>
-            </p>
-          </div>
-          <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
-            <a
-              href="https://segzyp1000.github.io/hngtask2/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img src={Project2} className="imgg" alt="" />
-            </a>
-            <p className="flex flex-col items-center w-1/2 mt-5">
-              Created a data-driven movie website using React, JavaScript, HTML,
-              and CSS, demonstrating my skills in building responsive and
-              dynamic web applications with a focus on data visualization.
-              <a
-                href="https://segzyp1000.github.io/hngtask2/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-white text-black rounded p-1 mt-3"
-              >
-                Click here
-              </a>
-            </p>
-          </div>
-          <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
+        </div>
+        <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
+          <a
+            href="https://logisticstransport.onrender.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Project3} alt="" />
+          </a>
+          <p className="flex flex-col justify-center items-center w-1/2 mt-5">
+            Developed a transport logistics website replica with Tailwind, HTML,
+            CSS, and JavaScript, demonstrating my skills in building responsive,
+            dynamic, and modern web applications
             <a
               href="https://logisticstransport.onrender.com/"
               target="_blank"
               rel="noopener noreferrer"
+              className="bg-white text-black rounded p-1 mt-3"
             >
-              <img src={Project3} alt="" />
+              Click here
             </a>
-            <p className="flex flex-col justify-center items-center w-1/2 mt-5">
-              Developed a transport logistics website replica with Tailwind,
-              HTML, CSS, and JavaScript, demonstrating my skills in building
-              responsive, dynamic, and modern web applications
-              <a
-                href="https://logisticstransport.onrender.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="bg-white text-black rounded p-1 mt-3"
-              >
-                Click here
-              </a>
-            </p>
-          </div>
+          </p>
+        </div>
+        <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
+          <a
+            href="https://segzyp1000.github.io/loansite/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Project1} alt="" />
+          </a>
+          <p className="flex flex-col items-center w-1/2 mt-5">
+            Built a loan website replica using React, JavaScript, HTML, CSS, and
+            Bootstrap, demonstrating my skills in creating responsive and
+            dynamic web applications
+            <a
+              href="https://segzyp1000.github.io/loansite"
+              className="bg-white text-black rounded p-1 mt-3"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Click here
+            </a>
+          </p>
+        </div>
+        <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
+          <a
+            href="https://segzyp1000.github.io/hngtask2/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Project2} className="imgg" alt="" />
+          </a>
+          <p className="flex flex-col items-center w-1/2 mt-5">
+            Created a data-driven movie website using React, JavaScript, HTML,
+            and CSS, demonstrating my skills in building responsive and dynamic
+            web applications with a focus on data visualization.
+            <a
+              href="https://segzyp1000.github.io/hngtask2/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-white text-black rounded p-1 mt-3"
+            >
+              Click here
+            </a>
+          </p>
         </div>
       </div>
     </div>
