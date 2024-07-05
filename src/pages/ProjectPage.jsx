4491070238ae653bfebca2d33f585b0b97c053c6
segun olowoyeye <segunolowoyeye@gmail.com>
2024-07-05 23:09:21 +0100

    firstly down

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -17,19 +17,31 @@ const ProjectPage = () => {
           >
             <img src={Project0} alt="" />
           </a>
-          <p className="flex flex-col items-center w-1/2 mt-5">
-            Designed and developed a responsive banana bread ordering website,
-            implementing Context API for efficient state management and ensuring
-            a smooth user interface
-            <a
-              href="https://dariola-banana-bread.vercel.app/"
-              className="bg-white text-black rounded p-1 mt-3"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Click here
-            </a>
-          </p>
+          <div className="flex flex-col items-center w-1/2 mt-5">
+            <p>
+              Designed and developed a responsive banana bread ordering website,
+              implementing Context API for efficient state management and
+              ensuring a smooth user interface
+            </p>
+            <div className="flex gap-2">
+              <a
+                href="https://dariola-banana-bread.vercel.app/"
+                className="bg-white text-black rounded p-1 mt-3"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Preview
+              </a>
+              <a
+                href="https://github.com/Segzyp1000/dariolaBananaBread"
+                className="bg-bgBackground text-white rounded p-1 mt-3"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View code
+              </a>
+            </div>
+          </div>
         </div>
         <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
           <a
@@ -39,19 +51,31 @@ const ProjectPage = () => {
           >
             <img src={Project3} alt="" />
           </a>
-          <p className="flex flex-col justify-center items-center w-1/2 mt-5">
-            Developed a transport logistics website replica with Tailwind, HTML,
-            CSS, and JavaScript, demonstrating my skills in building responsive,
-            dynamic, and modern web applications
-            <a
-              href="https://logisticstransport.onrender.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-white text-black rounded p-1 mt-3"
-            >
-              Click here
-            </a>
-          </p>
+          <div className="flex flex-col justify-center items-center w-1/2 mt-5">
+            <p>
+              Developed a transport logistics website replica with Tailwind,
+              HTML, CSS, and JavaScript, demonstrating my skills in building
+              responsive, dynamic, and modern web applications
+            </p>
+            <div className="flex gap-2">
+              <a
+                href="https://logisticstransport.onrender.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-white text-black rounded p-1 mt-3"
+              >
+                Preview
+              </a>
+              <a
+                href="https://github.com/Segzyp1000/logisticstra/tree/main/build"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-bgBackground text-white rounded p-1 mt-3"
+              >
+                View code
+              </a>
+            </div>
+          </div>
         </div>
         <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
           <a
@@ -61,19 +85,31 @@ const ProjectPage = () => {
           >
             <img src={Project1} alt="" />
           </a>
-          <p className="flex flex-col items-center w-1/2 mt-5">
-            Built a loan website replica using React, JavaScript, HTML, CSS, and
-            Bootstrap, demonstrating my skills in creating responsive and
-            dynamic web applications
-            <a
-              href="https://segzyp1000.github.io/loansite"
-              className="bg-white text-black rounded p-1 mt-3"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Click here
-            </a>
-          </p>
+          <div className="flex flex-col items-center w-1/2 mt-5">
+            <p>
+              Built a loan website replica using React, JavaScript, HTML, CSS,
+              and Bootstrap, demonstrating my skills in creating responsive and
+              dynamic web applications
+            </p>
+            <div className="flex gap-2">
+              <a
+                href="https://segzyp1000.github.io/loansite"
+                className="bg-white text-black rounded p-1 mt-3"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Preview
+              </a>
+              <a
+                href="https://segzyp1000.github.io/loansite"
+                className="bg-bgBackground text-white rounded p-1 mt-3"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View code
+              </a>
+            </div>
+          </div>
         </div>
         <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
           <a
@@ -83,19 +119,31 @@ const ProjectPage = () => {
           >
             <img src={Project2} className="imgg" alt="" />
           </a>
-          <p className="flex flex-col items-center w-1/2 mt-5">
-            Created a data-driven movie website using React, JavaScript, HTML,
-            and CSS, demonstrating my skills in building responsive and dynamic
-            web applications with a focus on data visualization.
-            <a
-              href="https://segzyp1000.github.io/hngtask2/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="bg-white text-black rounded p-1 mt-3"
-            >
-              Click here
-            </a>
-          </p>
+          <div className="flex flex-col items-center w-1/2 mt-5">
+            <p>
+              Created a data-driven movie website using React, JavaScript, HTML,
+              and CSS, demonstrating my skills in building responsive and
+              dynamic web applications with a focus on data visualization.
+            </p>
+            <div className="flex gap-2">
+              <a
+                href="https://segzyp1000.github.io/hngtask2/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-white text-black rounded p-1 mt-3"
+              >
+                Preview
+              </a>
+              <a
+                href="https://github.com/Segzyp1000/hngtask2"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-bgBackground text-white rounded p-1 mt-3"
+              >
+                View code
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
