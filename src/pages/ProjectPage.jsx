6627b9dd72ec2e567ecdb3908d4220c32c542d1f
segun olowoyeye <segunolowoyeye@gmail.com>
2024-07-26 21:08:57 +0100

    updated some features

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Project0 from "../assets/project11.png";
-import Project1 from "../assets/Project1.png";
 import Project2 from "../assets/Project2.png";
 import Project3 from "../assets/Project3.jpg";
 import Projec from "../assets/timbo-shop.png";
+import Project4 from "../assets/project4.png";
 
 const ProjectPage = () => {
   return (
@@ -38,7 +38,7 @@ const ProjectPage = () => {
                 Preview
               </a>
               <a
-                href="https://github.com/Segzyp1000/timbo-shop/tree/main/src"
+                href="https://github.com/Segzyp1000/timbo-shop"
                 className="bg-bgBackground text-white rounded p-2 mt-3 hover:bg-gray-500"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -108,7 +108,7 @@ const ProjectPage = () => {
                 Preview
               </a>
               <a
-                href="https://github.com/Segzyp1000/logisticstra/tree/main/build"
+                href="https://github.com/Segzyp1000/logisticstra"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="bg-bgBackground text-white rounded p-2 mt-3 hover:bg-gray-500"
@@ -147,6 +147,42 @@ const ProjectPage = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 className="bg-bgBackground text-white rounded p-2 mt-3 hover:bg-gray-500"
+              >
+                View code
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="separator"></div>
+        <div className="mt-10 flex flex-wrap justify-center items-center text-white text-sm space-x-2">
+          <a
+            href="https://segun-olowoyeye-lendsqr-fe-test.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Project4} alt="" />
+          </a>
+          <div className="flex flex-col items-center w-1/2 mt-5">
+            <p className="text-black">
+              "Built a replica of the Lendsqr dashboard website using React and
+              TypeScript, with styling achieved through SCSS. Additionally,
+              implemented pagination functionality utilizing React Router Dom,
+              enhancing the user experience through seamless navigation."
+            </p>
+            <div className="flex gap-2">
+              <a
+                href="https://segun-olowoyeye-lendsqr-fe-test.vercel.app/"
+                className="bg-white text-bgBackground rounded p-2 mt-3 hover:bg-zinc-500"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Preview
+              </a>
+              <a
+                href="https://github.com/Segzyp1000/lendsqr-fe-test"
+                className="bg-bgBackground text-white rounded p-2 mt-3 hover:bg-gray-500"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 View code
               </a>
